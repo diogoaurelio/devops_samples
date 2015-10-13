74003375bd046884cfdd4e15c13d015a07b79737
diogoaurelio <diogopaurelio@gmail.com>
2015-10-13 15:23:18 +0200

    corrected networking issue - launching VMs with public IP address and private IP address, and within a specific VPC

--- cloudera_cluster_lab/boto_provision_cloudera_lab.py
+++ cloudera_cluster_lab/boto_provision_cloudera_lab.py
@@ -2,6 +2,7 @@
 """
 Version: 0.1
 Description: Basic Recipe for provisioning AWS EC2 instances for a Cloudera Cluster lab.
+Notes: make sure you have VPC and subnets provisioned before you provision ec2 instances
 Improvements: Include Fabric/Ansible to provision the code in the VMs + provision more than one disk per hadoop node
 Authors: Diogo
 
@@ -26,18 +27,20 @@ else:
 my_env = "test"
 
 #REGION NAME
-region = 'eu-west-1'
+region = 'eu-central-1'
 
-my_vpc = { 'id' : 'vpc-907007f5', 'name': 'berlin_smart_data_vpc', 'cidr_block': '172.16.0.0/16' }
+#NOTE: for this region only AVZ: A & B available currently!
+
+my_vpc = { 'id' : 'vpc-17eb537e', 'name': 'berlin_smart_data_vpc', 'cidr_block': '172.16.0.0/16' }
 my_subnets = { 'A': {
-				'id': 'subnet-c9eaa8be', 'cidr_block': '172.16.5.0/24', 'availability_zone': region+'a' 
+				'id': 'subnet-2d33bd44', 'cidr_block': '172.16.5.0/24', 'availability_zone': region+'a' 
 				},
 				'B': {
-				'id': 'subnet-4bdcb812', 'cidr_block': '172.16.15.0/24', 'availability_zone': region+'b' 
+				'id': 'subnet-0f1cb374', 'cidr_block': '172.16.15.0/24', 'availability_zone': region+'b' 
 				},
-				'C': {
-				'id': 'subnet-d56246b0', 'cidr_block': '172.16.25.0/24', 'availability_zone': region+'c' 
-				}
+				#'C': {
+				#'id': 'new', 'cidr_block': '172.16.25.0/24', 'availability_zone': region+'c' 
+				#}
 			 } 
 
 
@@ -45,12 +48,14 @@ my_subnets = { 'A': {
 
 ###REMINDER: do NOT forget to make sure you HAVE the SSH KEY that you specify...
 
-ec2Group = collections.namedtuple("ec2Group", [ "id", "image_id", "key_name", "instance_type", "security_groups", "subnet_id", "region", "private_ip_address", "monitoring_enabled", "disable_api_termination", "name", "additional_info", "volumes" ])
-#NOTE: PAY ATTENTION TO SUBNET STATICALLY ASSIGNED MATCHES SUBNET ID (yes, lazzyness sometimes good..)
+ec2Group = collections.namedtuple("ec2Group", [ "name", "additional_info", "id", "image_id", "key_name", "instance_type", "security_groups", "subnet_id", "region", "private_ip_address", "monitoring_enabled", "disable_api_termination", "volumes" ])
+#NOTE - laziness side-effects gotchas: PAY ATTENTION TO SUBNET STATICALLY ASSIGNED MATCHES SUBNET ID + check AMI specific for Region: https://cloud-images.ubuntu.com/locator/ec2/
+#For region eu-west-1: ami => ami-b05101c7, Ubuntu 14.04 LTS (PV), trusty
+
 ec2Instances = [ 
-				ec2Group('new', 'ami-b05101c7', 'bsd_labs', 'm3.medium', 'bsd_cloudera_manager', my_subnets['A']['id'], region, '172.16.5.10/24', False, False, 'Cloudera_Manager', 'Cloudera Manager', [50]), # Ubuntu 14.04, x64, eu-west-1			
-				ec2Group('new', 'ami-b05101c7', 'bsd_labs', 'm3.large', 'bsd_cloudera_manager', my_subnets['A']['id'], region, '172.16.5.100/24', False, False, 'Cloudera_Node_1', 'Node 1', [60]), # Ubuntu 14.04, x64, eu-west-1
-				ec2Group('new', 'ami-b05101c7', 'bsd_labs', 'm3.large', 'bsd_cloudera_manager', my_subnets['B']['id'], region, '172.16.15.101/24', False, False, 'Cloudera_Node_2', 'Node 2', [60]) # Ubuntu 14.04, x64, eu-west-1
+				ec2Group('Cloudera_Manager', 'Cloudera Manager', 'new', 'ami-b6cff2ab', 'bsd_labs_eu_central_1', 'm3.medium', 'bsd_cloudera_manager', my_subnets['A']['id'], region, '172.16.5.10', False, False,  [50]), # Ubuntu 14.04, x64, eu-west-1			
+				ec2Group('Cloudera_Node_1', 'Node 1', 'new', 'ami-b6cff2ab', 'bsd_labs_eu_central_1', 'm3.large', 'bsd_cloudera_manager', my_subnets['A']['id'], region, '172.16.5.100', False, False, [60]), # Ubuntu 14.04, x64, eu-west-1
+				ec2Group('Cloudera_Node_2', 'Node 2', 'new', 'ami-b6cff2ab', 'bsd_labs_eu_central_1', 'm3.large', 'bsd_cloudera_manager', my_subnets['B']['id'], region, '172.16.15.101', False, False, [60]) # Ubuntu 14.04, x64, eu-west-1
 				]
 
 #SECURITY GROUPS
@@ -77,8 +82,9 @@ HADOOP_RULES = [
 		SecurityGroupRule("tcp", "60010", "60010", "0.0.0.0/0", "bsd_hadoop_nodes"), #HBase Web UI 
 		SecurityGroupRule("tcp", "25020", "25020", "0.0.0.0/0", "bsd_hadoop_nodes"), #Impala Castalogue Web UI 
 		SecurityGroupRule("tcp", "25010", "25010", "0.0.0.0/0", "bsd_hadoop_nodes"), #Impala StateStore Web UI	
-		SecurityGroupRule("tcp", "7182", "7182", "172.16.0.0/16", "bsd_hadoop_nodes"), # Agent heartbeat
-    	SecurityGroupRule("icmp", "-1", "-1", "172.16.0.0/16", "bsd_hadoop_nodes"), #Ping echo
+		#SecurityGroupRule("tcp", "7182", "7182", "172.16.0.0/16", "bsd_hadoop_nodes"), # Agent heartbeat
+		SecurityGroupRule("tcp", "0", "65535", my_vpc['cidr_block'], "bsd_hadoop_nodes"),
+    	SecurityGroupRule("icmp", "-1", "-1", my_vpc['cidr_block'], "bsd_hadoop_nodes"), #Ping echo
 ]
 
 
@@ -266,7 +272,8 @@ def create_ec2_instance(env, conn, vpc, ec2Group):
 			# 		conn.attach_volume (vol.id, inst.id, "/dev/sdx")
 			sg_id = [ sg.id for sg in vpc.get_all_security_groups() if sg.name == ec2.security_groups ]
 			if len(sg_id) > 0:
-				interface = boto.ec2.networkinterface.NetworkInterfaceSpecification(subnet_id = ec2.subnet_id, groups= sg_id, associate_public_ip_address = True)
+				#print ec2.private_ip_address
+				interface = boto.ec2.networkinterface.NetworkInterfaceSpecification(subnet_id = ec2.subnet_id, private_ip_address= ec2.private_ip_address, groups= sg_id, associate_public_ip_address = True)
 				interfaces = boto.ec2.networkinterface.NetworkInterfaceCollection(interface)
 				print "Creating new ec2 instance - " + str(ec2)
 				try:
@@ -277,6 +284,7 @@ def create_ec2_instance(env, conn, vpc, ec2Group):
                                 				#instead at the global level !!
 												#security_group_ids=sg_id, 
 												#subnet_id = ec2.subnet_id, 
+												network_interfaces = interfaces,
 												monitoring_enabled=ec2.monitoring_enabled, 
 												disable_api_termination= ec2.disable_api_termination, 
 												additional_info=ec2.additional_info, 
@@ -289,12 +297,12 @@ def create_ec2_instance(env, conn, vpc, ec2Group):
 							print instance, instance.state
 							time.sleep(5)
 							instance.update()
-						if instance.state != 'failed':
-							new_instaces_ids[ new_ec2.id ] = ec2.additional_info
+						if instance.state == 'running':
+							new_instaces_ids[ new_ec2.instances[0] ] = ec2.additional_info
 							instance.add_tag(ec2.name, ec2.additional_info)
 						else:
 							print "There was a problem, and instance could not be created.."
-							print Instance
+							print instance
 				except Exception, e:
 					print e
 				
@@ -308,31 +316,39 @@ def create_ec2_instance(env, conn, vpc, ec2Group):
 	else:
 		print "No new instances were provisioned. Alright, job completed!!"
 
-def networking(env, vpc):
+def provision_vpc(env, vpc):
 	dryRun = True
 	if env != "test":
 		dryRun = False
-	count_new_subnets = 0
 	print "Availability zones: " + str( vpc.get_all_zones() )
 	existing_vpcs = [v for v in vpc.get_all_vpcs() if v.id == my_vpc['id'] ]
 	existing_subnets = [s.id for s in vpc.get_all_subnets() ]
 	print "Info: Existing VPCs: " + str(vpc.get_all_vpcs())
 	print "Info: Existing Subnets: " + str(existing_subnets)
-	print my_vpc['id']
+	#print my_vpc['id']
 
 	vpc_to_prov = existing_vpcs[0] if existing_vpcs else None
 	if not vpc_to_prov:
 		print "Starting new VPC provisioning process.."
 		new_vpc = vpc.create_vpc(my_vpc['cidr_block'], dry_run = dryRun)
 		print "New VPC provisioned, please update its ID: " +str(new_vpc.id)
+		provision_subnets(env, vpc, new_vpc.id, existing_subnets)
 	else:
 		print "vpc already exists, nothing to do here, moving on to subnets.."
+		provision_subnets(env, vpc, my_vpc['id'], existing_subnets)
 	
+	
+
+def provision_subnets(env, vpc, vpc_id, existing_subnets):
+	dryRun = True
+	if env != "test":
+		dryRun = False
+	count_new_subnets = 0
 	for id, subnet in my_subnets.items():
 		print "Checking id: %s and subnet: %s" % (id, subnet)
 		if subnet['id'] not in existing_subnets:
 			count_new_subnets +=1
-			new_subnet = vpc.create_subnet(my_vpc['id'], subnet['cidr_block'], availability_zone= subnet['availability_zone'], dry_run = dryRun)
+			new_subnet = vpc.create_subnet(vpc_id, subnet['cidr_block'], availability_zone= subnet['availability_zone'], dry_run = dryRun)
 			print "New Subnet with id %s provisioned, please update its ID." % (str(new_subnet.id), )
 		else:
 			print "Subnet %s already exists.." % subnet['id']	
@@ -341,11 +357,16 @@ def networking(env, vpc):
 	else:
 		print "A total of %s new subnets were provisioned."%(str(count_new_subnets),)
 
+
 def boostrapper(env):
 	"""
 		Main launcher
 		@env - fake variable just to distinguish between testing and prod
     """
+	#from util import aws_conf
+	#conn = boto.ec2.connect_to_region(aws_conf['AWS_REGION'],
+    #                              aws_access_key_id=aws_conf['AWS_ACCESS_KEY_ID'],
+    #                              aws_secret_access_key=aws_conf['AWS_SECRET_ACCESS_KEY'])
 
 	#Booting up..
 	conn = boto.ec2.connect_to_region(region)
@@ -354,7 +375,7 @@ def boostrapper(env):
 	vpc = boto.vpc.connect_to_region(region)
 
 	try:
-		networking("production", vpc)
+		provision_vpc("production", vpc)
 	except Exception, e:
 		print e
 
@@ -369,7 +390,7 @@ def boostrapper(env):
 	#Fire new ec2 instances
 	print "Current ec2 instances:"
 	list_all_ec2_instances(env, conn)
-	create_ec2_instance("production", conn, vpc, ec2Instances)
+	create_ec2_instance(env, conn, vpc, ec2Instances)
 
 
 if __name__=="__main__":
